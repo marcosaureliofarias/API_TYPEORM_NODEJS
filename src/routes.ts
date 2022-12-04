95140dee9b5ef45f5e959e98d5f5743003c3358c
Marcos aurelio Aurelio <marcosaureliofarias2023@gmail.com>
2022-12-04 12:50:38 -0300

    create video categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { CreateVideoController } from "./controllers/CreateVideoController";
 import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
 import { UpdateCategoryController } from "./controllers/UpdateCategoryController";
@@ -10,5 +11,6 @@ routes.post("/categories", new CreateCategoryController().handle);
 routes.get("/categories", new GetAllCategoriesController().handle);
 routes.delete("/categories/:id", new DeleteCategoryController().handle);
 routes.put("/categories/:id", new UpdateCategoryController().handle);
+routes.post("/videos", new CreateVideoController().handle);
 
 export {routes }
